0701fa4c7b1d4c1dca5d3fa3f53bf922493b1017
Christopher Cahill <cthomascahill@gmail.com>
2018-11-02 11:18:22 -0700

    determine price based upon size of pie

--- pizzaTime/js/scripts.js
+++ pizzaTime/js/scripts.js
@@ -2,13 +2,15 @@ function Pizza (size, cheese, sauce, toppings) {
   this.size = size,
   this.cheese = cheese,
   this.sauce = sauce,
-  this.toppings = toppings
+  this.toppings = toppings,
+  this.price = 0
 }
 
 function Orders() {
   this.pizzas = [];
   this.currentId = 0;
 }
+
 
 Orders.prototype.addOrder = function(pizza) {
   this.pizzas.push(pizza);
@@ -21,12 +23,17 @@ Orders.prototype.assignId = function() {
 }
 
 
-
 var theseOrders = new Orders;
 
-
-
-
+var pizzaCost = function(pizza) {
+  for (i=0; i<theseOrders.pizzas.length; i++)
+  if (theseOrders.pizzas[i].size === "XL") {
+    theseOrders.pizzas[i].price = 5
+  }
+  else if (theseOrders.pizzas[i].size === "L") {
+    theseOrders.pizzas[i].price = 3
+  }
+}
 
 // UI
 
@@ -41,12 +48,14 @@ $(function() {
     var whatToppings = $("#multipleToppings").val();
     console.log(whatToppings);
 
-    theseOrders.addOrder(new Pizza(whatSize,whatCheese,whatSauce,whatToppings))
+    theseOrders.addOrder(new Pizza(whatSize,whatCheese,whatSauce,whatToppings));
 
-
-    console.log(theseOrders);
 
   });
 
 
-});
+
+    console.log(theseOrders);
+
+
+  });
