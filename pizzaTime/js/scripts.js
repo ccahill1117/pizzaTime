98ae2ea7f6bb333728bf0fb638e9eb93bb8e2c70
Christopher Cahill <cthomascahill@gmail.com>
2018-11-02 16:11:42 -0700

    adds order total function and additional formatting

--- pizzaTime/js/scripts.js
+++ pizzaTime/js/scripts.js
@@ -91,17 +91,25 @@ function displayPizzas(pizza) {
   var pizzaList = $("ul#pizzaSpace");
   var htmlForPizzaInfo = "";
   pizza.pizzas.forEach(function(pizza) {
-    htmlForPizzaInfo += "<li id=" + pizza.pizzaId + ">" + "Item No: " + pizza.pizzaId + " Size: " + pizza.size + " Cheese Amount: " + pizza.cheese + " Selected Toppings: " + pizza.toppings + " Item Price $" + pizza.price + "</li>";
+    htmlForPizzaInfo += "<li id=" + pizza.pizzaId + ">" + "🍕" + "Item No: " + pizza.pizzaId + " Size: " + pizza.size + " Cheese Amount: " + pizza.cheese + " Selected Toppings: " + pizza.toppings + " Item Price $" + pizza.price + "</li>";
   });
   pizzaList.html(htmlForPizzaInfo);
 };
+
+function displayOrderAmount(pizza) {
+  var orderAmount = 0;
+  for (i=0; i<theseOrders.pizzas.length;i++)
+  orderAmount += theseOrders.pizzas[i].price;
+  return orderAmount;
+  }
+
 
 function displayPizzasTotal(pizzaId) {
   var pizza = theseOrders.findPizza(pizzaId);
   $("#show-pizza").show();
   $("#sizePiz").text(pizza.size);
-
 };
+
 
 function attachContactListeners() {
   $("ul#pizzaSpace").on("click", "li", function() {
@@ -127,6 +135,8 @@ $(document).ready(function() {
     toppingsCost(theseOrders,(theseOrders.pizzas.length-1));
 
     displayPizzas(theseOrders);
+    $("#orderTotal").text(displayOrderAmount(theseOrders));
+
 
     console.log(theseOrders);
   });
