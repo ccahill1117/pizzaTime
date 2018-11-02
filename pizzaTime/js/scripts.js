67dcf3d0834049e0fcc5c5c8216b15968a957155
Christopher Cahill <cthomascahill@gmail.com>
2018-11-02 11:32:27 -0700

    add pricing logic and increment pizzaPrice based upon toppings

--- pizzaTime/js/scripts.js
+++ pizzaTime/js/scripts.js
@@ -1,7 +1,6 @@
-function Pizza (size, cheese, sauce, toppings) {
+function Pizza (size, cheese, toppings) {
   this.size = size,
   this.cheese = cheese,
-  this.sauce = sauce,
   this.toppings = toppings,
   this.price = 0
 }
@@ -19,19 +18,50 @@ Orders.prototype.addOrder = function(pizza) {
 }
 
 Orders.prototype.assignId = function() {
-  return this.currentId += 1;
+  return this.pizzaID += 1;
 }
-
 
 var theseOrders = new Orders;
 
-var pizzaCost = function(pizza) {
+var sizeCost = function(pizza) {
   for (i=0; i<theseOrders.pizzas.length; i++)
   if (theseOrders.pizzas[i].size === "XL") {
-    theseOrders.pizzas[i].price = 5
+    theseOrders.pizzas[i].price += 10
   }
   else if (theseOrders.pizzas[i].size === "L") {
-    theseOrders.pizzas[i].price = 3
+    theseOrders.pizzas[i].price += 7
+  }
+  else if (theseOrders.pizzas[i].size === "M") {
+    theseOrders.pizzas[i].price += 5
+  }
+}
+
+var cheeseCost = function(pizza) {
+  for (i=0; i<theseOrders.pizzas.length; i++)
+  if (theseOrders.pizzas[i].cheese === "extra cheese") {
+    theseOrders.pizzas[i].price += 2
+  }
+  else if (theseOrders.pizzas[i].cheese === "normal") {
+    theseOrders.pizzas[i].price += 0
+  }
+}
+
+var toppingsCost = function(pizza) {
+  for (i=0; i<theseOrders.pizzas.length; i++)
+  if (theseOrders.pizzas[i].toppings.length === 1) {
+    theseOrders.pizzas[i].price += 1
+  }
+  else if (theseOrders.pizzas[i].toppings.length === 2) {
+    theseOrders.pizzas[i].price += 2
+  }
+  else if (theseOrders.pizzas[i].toppings.length === 3) {
+    theseOrders.pizzas[i].price += 3
+  }
+  else if (theseOrders.pizzas[i].toppings.length === 4) {
+    theseOrders.pizzas[i].price += 4
+  }
+  else if (theseOrders.pizzas[i].toppings.length === 5) {
+    theseOrders.pizzas[i].price += 5
   }
 }
 
@@ -44,18 +74,22 @@ $(function() {
 
     var whatSize = $("#sizeOption").val();
     var whatCheese = $("#cheeseOption").val();
-    var whatSauce = $("#sauceOption").val();
     var whatToppings = $("#multipleToppings").val();
-    console.log(whatToppings);
 
-    theseOrders.addOrder(new Pizza(whatSize,whatCheese,whatSauce,whatToppings));
+    theseOrders.addOrder(new Pizza(whatSize,whatCheese,whatToppings));
+
+    sizeCost(theseOrders);
+    cheeseCost(theseOrders);
+    toppingsCost(theseOrders);
+
+    console.log(theseOrders);
+
 
 
   });
 
 
 
-    console.log(theseOrders);
 
 
   });
